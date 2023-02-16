118e5a46240d62238ceafe9d94b2b477d41a1cf4
David Elbaz <102173191+DavidELBAZpro@users.noreply.github.com>
2023-02-16 11:58:39 +0100

    Added files via Upload

---
+++ src/root-cmp.tsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Sareden</a>
+        <img
+          src="//cdn.shopify.com/s/files/1/0665/4774/4989/files/Logo-Header.gif?v=1673770152"
+          alt="Saréden"
+        ></img>
+      </header>
+      <main>list product</main>
+    </div>
+  )
+}
